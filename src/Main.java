1852ba106082eb6f25b0d946aae2547f6f269d73
Anastasia N <nastka_kapito@mail.ru>
2022-05-12 18:49:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 3000;
+        int miles = ticket / 20;
+
+        System.out.println("Бонусные мили" + miles);
+
+    }
+    }
